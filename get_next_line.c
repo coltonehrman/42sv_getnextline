cdb1c8be250f3d8368842bfecdabdfb14aae2920
Colton Ehrman <cehrman@e1z3r4p1.42.us.org>
2020-02-22 20:26:30 -0800

    updates

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: cehrman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/21 13:32:23 by cehrman           #+#    #+#             */
-/*   Updated: 2020/02/22 14:31:31 by cehrman          ###   ########.fr       */
+/*   Updated: 2020/02/22 20:26:05 by cehrman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,45 +36,78 @@ int		resize_data(char **data, int bytes_read, int total_bytes)
 	return (i);
 }
 
+int		copy_buff(char *buff, char **data, int *i, int total_bytes)
+{
+	int j;
+
+	j = 0;
+	while (*i < total_bytes)
+	{
+		if (buff[j] == '\n')
+		{
+			(*data)[*i] = 0;
+			return (1);
+		}
+		(*data)[*i] = buff[j];
+		(*i)++;
+		j++;
+	}
+	return (0);
+}
+
 int		get_next_line(const int fd, char **line)
 {
 	static int	reads = 0;
 	char		buff[BUFF_SIZE];
 	char		*data;
-	int			bytes_read;
-	int			total_bytes;
-	int			i;
-	int			j;
+	int			bytes[2];
+	int			i[2];
 
 	reads++;
 	data = 0;
-	bytes_read = 0;
-	total_bytes = 0;
-	if (!line)
-		return (0);
-	while ((bytes_read = read(fd, buff, BUFF_SIZE)) > 0)
+	bytes[1] = 0;
+	if (!line || fd < 0)
+		return (-1);
+	while ((bytes[0] = read(fd, buff, BUFF_SIZE)) > 0)
 	{
-		total_bytes += bytes_read;
-		i = resize_data(&data, bytes_read, total_bytes);
-		j = 0;
-		while (i < total_bytes)
-		{
-			if (buff[j] == '\n')
-			{
-				data[i] = 0;
-				*line = data;
-				return (1);
-			}
-			data[i] = buff[j];
-			i++;
-			j++;
-		}
-	}
-	if (total_bytes > 0)
-	{
-		i = resize_data(&data, bytes_read, bytes_read + 1);
-		data[i] = 0;
+		bytes[1] += bytes[0];
+		i[0] = resize_data(&data, bytes[0], bytes[1]);
+		if ((copy_buff(buff, &data, &(i[0]), bytes[1]) == 1))
+			return ((*line = data) ? 1 : 1);
 	}
 	*line = data;
-	return (total_bytes > 0);
+	if (bytes[1] > 0 && (i[0] = resize_data(&data, bytes[0], bytes[1])))
+		data[i[0]] = 0;
+	*line = data;
+	if (bytes[0] <= 0 && bytes[1] < 1)
+		return (bytes[0]);
+	return (bytes[1] > 0 ? 1 : -1);
 }
+
+/*
+#include <fcntl.h>
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+	int fd[2];
+	int res[2];
+	char *line[2];
+	fd[0] = open(argv[1], O_RDONLY);
+	
+	while ((res[0] = get_next_line(fd[0], &(line[0]))) > 0)
+	{
+		printf("fd: %d - %s\n", fd[0], line[0]);
+	}
+	close(fd[0]);
+
+	fd[1] = open(argv[2], O_RDONLY);
+	while ((res[1] = get_next_line(fd[1], &(line[1]))) > 0)
+	{
+		printf("fd: %d - %s\n", fd[1], line[1]);
+	}
+	close(fd[1]);
+
+	return (0);
+}
+*/
--- get_next_line.h
+++ get_next_line.h
@@ -6,14 +6,14 @@
 /*   By: cehrman <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/21 13:32:14 by cehrman           #+#    #+#             */
-/*   Updated: 2020/02/22 10:10:46 by cehrman          ###   ########.fr       */
+/*   Updated: 2020/02/22 19:45:38 by cehrman          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 # ifndef BUFFSIZE
-#  define BUFF_SIZE 1
+#  define BUFF_SIZE 1 
 # endif
 # include <stdlib.h>
 # include <unistd.h>
